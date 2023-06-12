a322f8b8be83736a31d1fa4c440953e410ddfe13
vm2124 <vm21242124@gmail.com>
2023-06-12 23:42:46 +0530

    jwt

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -4,12 +4,19 @@ import { asyncHandler } from "../services/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const isLoggedIn = asyncHandler(async (req, res, next) => {
-  let token=req.cookies.token ;
+  let token
+    if(req.cookies.token ||
+        req.headers.authorization && 
+        req.headers.startWith("Bearer")) {
+            token = req.cookies.token ||
+            req.headers.authorization.split(" ")[1]
+        }
+
     if(!token) {
         throw new CustomError("Not authorized to access the route1", 401)
     }
     try {
-        const decode =  jwt.verify(token,process.env.JWT_SECRET)
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
         req.user = await userModel.findById(decode._id, "name email role")
         next()
     } catch (error) {
