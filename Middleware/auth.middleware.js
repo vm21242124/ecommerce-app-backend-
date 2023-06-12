2ca3e2a2b8d650463d94260ea42baf427fcf0bd5
vm2124 <vm21242124@gmail.com>
2023-06-12 23:30:01 +0530

    middleware problem

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -4,21 +4,22 @@ import { asyncHandler } from "../services/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const isLoggedIn = asyncHandler(async (req, res, next) => {
-  let token;
-  if (
-    req.cookies.token ||
-    (req.headers.authorization && req.headers.startWith("Bearer"))
-  ) {
-    token = req.cookies.token || req.headers.authorization.split(" ")[1];
-  }
-  if (!token) {
-    throw new CustomError("User Not Logged In", 400);
-  }
-  try {
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await userModel.findById(decode._id);
-    next();
-  } catch (e) {
-    throw new CustomError("Not Allowed",401)   
-  }
+  let token
+    if(req.cookies.token ||
+        req.headers.authorization && 
+        req.headers.startWith("Bearer")) {
+            token = req.cookies.token ||
+            req.headers.authorization.split(" ")[1]
+        }
+
+    if(!token) {
+        throw new CustomError("Not authorized to access the route1", 401)
+    }
+    try {
+        const decode =  jwt.verify(token, config.JWT_SECRET)
+        req.user = await userModel.findById(decode._id, "name email role")
+        next()
+    } catch (error) {
+        throw new CustomError("Not authorized to access the route | error", 401)
+    }
 });
