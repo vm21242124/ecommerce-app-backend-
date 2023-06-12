711e9f2dbe71df12366b3273d5e1d07c9b6dc4d9
vm2124 <vm21242124@gmail.com>
2023-06-12 23:22:54 +0530

    middleware

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -1,24 +1,24 @@
 import { userModel } from "../Models/UserSchema.js";
 import CustomError from "../Utils/cutomError.js";
 import { asyncHandler } from "../services/asyncHandler.js";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-export const isLoggedIn = asyncHandler(async (req, res,next) => {
+export const isLoggedIn = asyncHandler(async (req, res, next) => {
   let token;
   if (
     req.cookies.token ||
     (req.headers.authorization && req.headers.startWith("Bearer"))
   ) {
-    token = req.cookies.token || req.headers.authorization.split("")[1];
+    token = req.cookies.token || req.headers.authorization.split(" ")[1];
   }
-  if(!token){
-    throw new CustomError("User Not Logged In",400)
+  if (!token) {
+    throw new CustomError("User Not Logged In", 400);
   }
   try {
-    const decode=jwt.verify(token,process.env.JWT_SECRET)
-    req.user=await userModel.findById(decode._id);
-    next()
-  } catch (error) {
-    throw new CustomError("Not authorize to access the route",401)
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await userModel.findById(decode._id);
+    next();
+  } catch (e) {
+    throw new CustomError("Not Allowed",401)   
   }
 });
