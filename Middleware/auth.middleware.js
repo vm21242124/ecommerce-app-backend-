f990200fe555a2c48ebdc76696671414a42e7b52
vm2124 <vm21242124@gmail.com>
2023-06-12 23:33:26 +0530

    token

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -4,14 +4,7 @@ import { asyncHandler } from "../services/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const isLoggedIn = asyncHandler(async (req, res, next) => {
-  let token
-    if(req.cookies.token ||
-        req.headers.authorization && 
-        req.headers.startWith("Bearer")) {
-            token = req.cookies.token ||
-            req.headers.authorization.split(" ")[1]
-        }
-
+  let token=req.cookies.token ;
     if(!token) {
         throw new CustomError("Not authorized to access the route1", 401)
     }
